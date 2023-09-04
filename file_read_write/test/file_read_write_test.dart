22d4d7dd40ae0a833a81e3a6735597fdff7d3d5c
Babul Mirdha <babumirdha@gmail.com>
2023-09-04 16:18:27 +0600

    Update file_read_write_test.dart

--- file_read_write/test/file_read_write_test.dart
+++ file_read_write/test/file_read_write_test.dart
@@ -1,8 +1,8 @@
-import 'package:file_read_write/file_read_write.dart';
-import 'package:test/test.dart';
+// import 'package:file_read_write/file_read_write.dart';
+// import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  // test('calculate', () {
+  //   expect(calculate(), 42);
+  // });
 }
