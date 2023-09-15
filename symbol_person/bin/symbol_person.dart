01ad9e1a8db1d04a78f9a7931897a957f9818091
Babul Mirdha <babumirdha@gmail.com>
2023-09-16 01:31:44 +0600

    Improved symbols

--- symbol_person/bin/symbol_person.dart
+++ symbol_person/bin/symbol_person.dart
@@ -2,6 +2,42 @@ import 'dart:core';
 import 'dart:mirrors';
 import '../lib/person.dart';
 
+
+void checkIfClassAvailableInLibrary(Symbol libraryName, Symbol className) async {
+  MirrorSystem mirrorSystem = currentMirrorSystem();
+  LibraryMirror libMirror = mirrorSystem.findLibrary(libraryName);
+
+  print("Found Library");
+  print("checking...class details..");
+  print("No of classes found is : ${libMirror.declarations.length}");
+  libMirror.declarations.forEach((s, d) => print(s));
+
+  if (libMirror.declarations.containsKey(className)) {
+    print("class found..");
+  }
+}
+
+
+void reflectInstanceMethods(Symbol libraryName, Symbol className) {
+  MirrorSystem mirrorSystem = currentMirrorSystem();
+  LibraryMirror libMirror = mirrorSystem.findLibrary(libraryName);
+
+  print("Found Library");
+  print("checking...class details..");
+  print("No of classes found is : ${libMirror.declarations.length}");
+  libMirror.declarations.forEach((s, d) => print(s));
+
+  if (libMirror.declarations.containsKey(className)) {
+    print("found class");
+  }
+  ClassMirror? classMirror = libMirror.declarations[className] as ClassMirror?;
+
+  print("No of instance methods found is ${classMirror?.instanceMembers.length}");
+  classMirror?.declarations.forEach((s, v) => print(s));
+}
+
+
+
 main() {
   //library name stored as Symbol
   Symbol librarySymbol = Symbol("person_lib");
@@ -9,19 +45,9 @@ main() {
   // class name stored as Symbol
   Symbol classPersonSymbol = Symbol("Person");
 
-  MirrorSystem mirrorSystem = currentMirrorSystem();
+ checkIfClassAvailableInLibrary(librarySymbol, classPersonSymbol);
 
-  LibraryMirror libMirror = mirrorSystem.findLibrary(librarySymbol);
-
-  print("No of classes found is : ${libMirror.declarations.length}");
-
-  libMirror.declarations.forEach((s, d) => print("s:$s, d.qualifiedName:${d.qualifiedName}"));
-
-  // searches Foo class in foo_lib library
-  if (libMirror.declarations.containsKey(classPersonSymbol))
-    {
-        print("class found..");
-    }
+  reflectInstanceMethods(librarySymbol, classPersonSymbol);
 }
 
 
