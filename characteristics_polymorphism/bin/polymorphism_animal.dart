b808dec27c3abb7eb65a4a5a879ab0fee0d7980a
Babul Mirdha <145245825+babulcorp@users.noreply.github.com>
2023-09-19 10:04:06 +0600

    Refactored

---
+++ characteristics_polymorphism/bin/polymorphism_animal.dart
@@ -0,0 +1,5 @@
+import 'package:polymorphism_animal/polymorphism_animal.dart' as polymorphism_animal;
+
+void main(List<String> arguments) {
+  print('Hello world: ${polymorphism_animal.calculate()}!');
+}
---
+++ characteristics_polymorphism/test/polymorphism_animal_test.dart
@@ -0,0 +1,8 @@
+import 'package:polymorphism_animal/polymorphism_animal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
