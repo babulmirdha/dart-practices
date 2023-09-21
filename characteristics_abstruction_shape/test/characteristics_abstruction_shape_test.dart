290cb897c91302f63b762d8850c4a18dad177676
babulcorp <babul.corp@gmail.com>
2023-09-21 16:47:48 +0600

    commit

--- characteristics_abstruction_shape/test/characteristics_abstruction_shape_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:characteristics_abstruction_shape/characteristics_abstruction_shape.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
